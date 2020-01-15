2958c789b3f629e39c99822d7da1aa6c12de4e1f
hudgit2019 <903094609@qq.com>
2020-01-15 18:50:58 +0800

    version modified

--- gameboot/version.go
+++ gameboot/version.go
@@ -4,7 +4,7 @@ import (
 	"github.com/name5566/leaf/log"
 )
 
-const LEAFBOOT_VERSION = "20.1.14.01"
+const LEAFBOOT_VERSION = "20.1.15.01"
 
 func init() {
 	log.Debug("LeafBoot:%v initialized", LEAFBOOT_VERSION)
